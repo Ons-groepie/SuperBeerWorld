8080303c3b84f965788ca695c7e2f089dcda9ed7
MartijnHemelt <martijn.hemelt@gmail.com>
2015-06-11 12:52:23 +0200

    Character

--- SuperBeerWorld/Character.cs
+++ SuperBeerWorld/Character.cs
@@ -8,6 +8,54 @@ namespace SuperBeerWorld
 {
     class Character
     {
+        String naam;
+        int AP;
+        int levens;
+        int statiegeld;
+        int xpos;
+        int ypos;
+        String afbeelding;
+        int speed;
+        int jumpheight;
 
+        public void setAP(int AP)
+        {
+            
+        }
+
+        public void setLevens
+        {
+
+        }
+
+        public void Statiegeld
+        {
+
+        }
+
+        private void setYpos
+        {
+
+        }
+
+        private void setXpos
+        {
+
+        }
+
+        private void setAfbeelding
+        {
+
+        }
+
+        private void setSpeed
+        {
+
+        }
+
+        private void setJumpHeight
+        {
+
+        }
     }
 }
