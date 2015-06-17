b427f96471bdf5843df19f3d1ed6ca4a1d671ea7
Falco Hulzebos <falcohulzebos@gmail.com>
2015-06-17 12:40:54 +0200

    lol was ff nodig voor mrge

--- SuperBeerWorld/Character.cs
+++ SuperBeerWorld/Character.cs
@@ -1,69 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace SuperBeerWorld
-{
-    class Character
-    {
+//namespace SuperBeerWorld
+//{
+//    class Character
+//    {
 
-        String naam;
-        int AP;
-        int levens;
-        int statiegeld;
-        int xpos;
-        int ypos;
-        String afbeelding;
-        int speed;
-        int jumpheight;
+//        String naam;
+//        int AP;
+//        int levens;
+//        int statiegeld;
+//        int xpos;
+//        int ypos;
+//        String afbeelding;
+//        int speed;
+//        int jumpheight;
 
-        public Character(String naam)
-        {
-            naam = this.naam;
-            AP = 0;
-            levens = 3;
-        }
+//        public Character(String naam)
+//        {
+//            naam = this.naam;
+//            AP = 0;
+//            levens = 3;
+//        }
 
-        public void setAP(int AP)
-        {
-            AP = this.AP;
-        }
+//        public void setAP(int AP)
+//        {
+//            AP = this.AP;
+//        }
 
-        public void setLevens
-        {
+//        public void setLevens
+//        {
 
-        }
+//        }
 
-        public void Statiegeld
-        {
+//        public void Statiegeld
+//        {
 
-        }
+//        }
 
-        private void setYpos
-        {
+//        private void setYpos
+//        {
 
-        }
+//        }
 
-        private void setXpos
-        {
+//        private void setXpos
+//        {
 
-        }
+//        }
 
-        private void setAfbeelding
-        {
+//        private void setAfbeelding
+//        {
 
-        }
+//        }
 
-        private void setSpeed
-        {
+//        private void setSpeed
+//        {
 
-        }
+//        }
 
-        private void setJumpHeight
-        {
+//        private void setJumpHeight
+//        {
 
-        }
-    }
-}
+//        }
+//    }
+//}
