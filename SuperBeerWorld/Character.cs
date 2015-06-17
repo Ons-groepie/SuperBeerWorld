48ad0813b5846328081e25c9cf175fbf88a45556
maaike1193 <maaikeveurink@hotmail.com>
2015-06-17 14:54:19 +0200

    facebook shit af

--- SuperBeerWorld/Character.cs
+++ SuperBeerWorld/Character.cs
@@ -8,62 +8,70 @@ namespace SuperBeerWorld
 {
     class Character
     {
-
         String naam;
+        String afbeelding;
+        String character;
         int AP;
         int levens;
         int statiegeld;
-        int xpos;
-        int ypos;
-        String afbeelding;
-        int speed;
-        int jumpheight;
 
+        // Default constructor
         public Character(String naam)
         {
-            naam = this.naam;
+            checkCharacter(this.naam);
+            naam = character;
             AP = 0;
+            statiegeld = 0;
             levens = 3;
         }
 
+        // Constructor met extra parameters
+        public Character(String naam, int statiegeld, int AP, int levens)
+        {
+            checkCharacter(this.naam);
+            naam = character;
+            AP = this.AP;
+            statiegeld = this.statiegeld;
+            levens = this.levens;
+        }
+
+        // Controleren of de ingegeven naam bestaat in onze game.
+        // Zo niet, dan default character gebruiken.
+        private void checkCharacter(String naam)
+        {
+            if (naam.ToLower() == "daniel" || naam.ToLower() == "jolt" || naam.ToLower() == "maaike" || naam.ToLower() == "falco" || naam.ToLower() == "martijn")
+            {
+                character = this.naam.ToLower();
+            }
+            else
+            {
+                // Set default character (Daniel)
+                character = "daniel";
+            }
+        }
+
+        /*
+         * 
+         *  Blokje met SET methodes
+         *  
+         */
+
+        // AP verhogen
         public void setAP(int AP)
         {
-            AP = this.AP;
+            AP = AP + this.AP;
         }
 
-        public void setLevens
+        // Levens instellen
+        public void setLevens(int levens)
         {
-
+            levens = this.levens;
         }
 
-        public void Statiegeld
+        // Statiegeld verhogen
+        public void setStatiegeld(int statiegeld)
         {
-
-        }
-
-        private void setYpos
-        {
-
-        }
-
-        private void setXpos
-        {
-
-        }
-
-        private void setAfbeelding
-        {
-
-        }
-
-        private void setSpeed
-        {
-
-        }
-
-        private void setJumpHeight
-        {
-
+            statiegeld = statiegeld + this.statiegeld;
         }
     }
 }
