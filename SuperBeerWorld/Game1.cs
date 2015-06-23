487ad1b5f738ea8bf9028320ffdd9c6d0d732d48
dvanmelzen <daniel.van.melzen@student.stenden.com>
2015-06-23 20:49:34 +0200

    Commit

--- SuperBeerWorld/Game1.cs
+++ SuperBeerWorld/Game1.cs
@@ -93,7 +93,7 @@ namespace SuperBeerWorld
                     button.Visibility = Visibility.Collapsed;
                     if (!class1.isPlayingMusic)
                     {
-                       MediaPlayer.Play(backgroundMusic1);
+                       //MediaPlayer.Play(backgroundMusic1);
                        class1.isPlayingMusic = true;
                     }
                     break;
@@ -102,7 +102,7 @@ namespace SuperBeerWorld
                     button.Visibility = Visibility.Visible;
                     if (!mainmenu.isPlayingMusic)
                     {
-                        MediaPlayer.Play(backgroundMusic2);
+                        //MediaPlayer.Play(backgroundMusic2);
                         mainmenu.isPlayingMusic = true;
                     }
                     break;
